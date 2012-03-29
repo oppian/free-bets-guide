937676f69a38af2e2c7d470009bd96dd15962c36
Matthew Jacobi <matt@oppian.com>
2012-03-29 12:19:18 +0100

    adding conf file to puppet

--- puppet/modules/free-bets-guide/manifests/init.pp
+++ puppet/modules/free-bets-guide/manifests/init.pp
@@ -3,7 +3,12 @@ class free-bets-guide {
   file {'/deploy/docroot/include/config.php':
       ensure  => file,
       content => template('free-bets-guide/config.php.erb'),
-    }
+  }
+
+  file {'/etc/httpd/conf.d/free-bets-guide.conf':
+      content => template('free-bets-guide/free-bets-guide.conf.erb'),
+      notify => Service['httpd'],
+  }
 
   class {"php":}
 
